9f5d9fe134bad7f45a93cac9d9d23ec5075e5e0e
totti-ue <osamu232@yahoo.co.jp>
2020-02-10 21:36:28 +0900

    create_group_edit_function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 
 end
