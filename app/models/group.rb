c50cd9cf82d20fae7f723dd6fbb72bcb46cff825
totti-ue <osamu232@yahoo.co.jp>
2020-02-11 15:22:47 +0900

    add_edit_group_function

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,3 +3,4 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true
 end
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :group_users
-  has_many :groups, through: :groups_users
+  has_many :groups, through: :group_users
 end
